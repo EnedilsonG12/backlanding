b04522e1a08aa44b95b93354f79d811fc08a95b3
Enedilson González <bernaldo.enedilson452001@gmail.com>
2025-10-19 10:54:56 -0600

    Corrigiendo Base de DAT....

--- index.js
+++ index.js
@@ -35,11 +35,11 @@ app.use(express.json());
 // DB Pool
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   connectionLimit: 10,
-  port: 3306
 });
 
 app.get('/', (req, res) => {
